7855aad6ec05ac589587bbd0f3f60b6515eddad0
tomtomduhamel <tomtom.duhamel@gmail.com>
2025-02-07 15:47:14 -0500

    fix: clean index.ts file

--- functions-v2/src/index.ts
+++ functions-v2/src/index.ts
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-=======
-
 import * as admin from 'firebase-admin';
 import { generateStory } from './handlers/storyHandler';
 
@@ -36,5 +14,3 @@ try {
 export {
   generateStory
 };
-
->>>>>>> 753fde53a2162a45e6de2b55ca5fc0a4fa2421d2
